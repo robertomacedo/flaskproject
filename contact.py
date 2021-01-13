5c62b7267255e086b02e8e6dfc3d0fdc45b48975
robertomacedo <macedorss20@gmail.com>
2021-01-13 09:34:43 -0300

    cirada um request local

--- contact.py
+++ contact.py
@@ -1,11 +1,16 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 
 bp = Blueprint('contatct', __name__, url_prefix='/contact')
 
-@bp.route('/', method=['GET', 'POST']) # Inserir GET e POST reconhece o form e seu get or post
+@bp.route('/', methods=['GET', 'POST']) # Inserir GET e POST reconhece o form e seu get or post
 def contact():
-    return render_template('contact.html')
+    if request.method == 'GET':
+        return render_template('contact.html')
+
+    # processa os dados do form
+    print(request.form)
+    return "Mensagem enviada com sucesso!"
 
 def configure(app):
     app.register_blueprint(bp)
